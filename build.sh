45220728257389281530fd921a31836c57b8789d
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-09 18:03:59 -0500

    Adding changes to build script

--- build.sh
+++ build.sh
@@ -23,19 +23,33 @@ cd pulp
 cd ..
 
 echo Creating initial RAM filesystem...
+rm -rf initrd
 mkdir initrd
 cd initrd
 mkdir lib
 mkdir bin
+mkdir src
 cp ../peel/lib/libpith.fap lib/libpith.asm
 cp ../peel/lib/_start.fap lib/_start.asm
 cp ../peel/bin/* bin
-cp -rp ../peel/src src
+cp -rp ../peel/src/* src
+mkdir src/pulp
+cp -rp ../pulp/src/* src/pulp
+mkdir src/yc
+cp -rp ../yuzu/src/yc/*.yuzu src/yc
+mkdir src/zest
+cp -rp ../yuzu/src/zest/*.yuzu src/zest
+cd src
+../../peel/bin/jar.fap c source.jar
+../../peel/bin/juicer.fap c source.jar ../source.jz
+cd ..
+rm -rf src
+#mkdir src
+#mv source.jz src
+rm source.jz
 ../peel/bin/jar.fap c initrd.jar
 cd ..
 
 cat seed/bin/fdseed.bin pulp/bin/pulp.sys initrd/initrd.jar > fruityos.img
 stat --printf="FruityOS size is %s bytes.\n" fruityos.img
 truncate -s 1440K fruityos.img
-
-rm -rf initrd
