d1852a5f150bd3d6aa2b3a60e92b78a79c70a0f6
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-10 13:10:07 -0400

    Preparing for merge

--- build.sh
+++ build.sh
@@ -1,22 +1,27 @@
 echo Building yuzu...
-cd yuzu
+cd yuzu/yuzu
 ./build.sh
+cd ..
 cd ..
 echo Building fapld...
-cd fapld
+cd fapld/fapld
 ./build.sh
+cd ..
 cd ..
 echo Building peel...
-cd peel
+cd peel/peel
 ./build.sh
+cd ..
 cd ..
 echo Building seed...
-cd seed
+cd seed/seed
 ./build.sh
 cd ..
+cd ..
 echo Building pulp...
-cd pulp
+cd pulp/pulp
 ./build.sh
+cd ..
 cd ..
 
 echo Creating initial RAM filesystem...
@@ -26,12 +31,12 @@ cd initrd
 mkdir lib
 mkdir bin
 mkdir src
-cp ../peel/bin/* bin
-cp ../peel/lib/libpith.fap lib/libpith.asm
-cp ../peel/lib/_start.fap lib/_start.asm
-../peel/bin/jar.fap c initrd.jar
+cp ../peel/peel/bin/* bin
+cp ../peel/peel/lib/libpith.fap lib/libpith.asm
+cp ../peel/peel/lib/_start.fap lib/_start.asm
+../peel/peel/bin/jar.fap c initrd.jar
 cd ..
 
-cat seed/bin/fdseed.bin pulp/bin/pulp.sys initrd/initrd.jar > fruityos.img
+cat seed/seed/bin/fdseed.bin pulp/pulp/bin/pulp.sys initrd/initrd.jar > fruityos.img
 stat --printf="FruityOS size is %s bytes.\n" fruityos.img
 truncate -s 1440K fruityos.img
