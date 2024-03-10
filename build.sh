d6cf796a8b63b2561163cde2000ff039ed9b0757
orby <orby@Ubuntu.myguest.virtualbox.org>
2024-03-10 11:28:19 -0400

    Cleaning up build process

--- build.sh
+++ build.sh
@@ -1,23 +1,20 @@
-sudo apt install nasm gcc gdb qemu-system-x86
-
-git clone git@github.com:fruityos/pulp.git
-git clone git@github.com:fruityos/yuzu.git
-git clone git@github.com:fruityos/peel.git
-git clone git@github.com:fruityos/fapld.git
-git clone git@github.com:fruityos/seed.git
-
+echo Building yuzu...
 cd yuzu
 ./build.sh
 cd ..
+echo Building fapld...
 cd fapld
 ./build.sh
 cd ..
+echo Building peel...
 cd peel
 ./build.sh
 cd ..
+echo Building seed...
 cd seed
 ./build.sh
 cd ..
+echo Building pulp...
 cd pulp
 ./build.sh
 cd ..
@@ -29,24 +26,9 @@ cd initrd
 mkdir lib
 mkdir bin
 mkdir src
+cp ../peel/bin/* bin
 cp ../peel/lib/libpith.fap lib/libpith.asm
 cp ../peel/lib/_start.fap lib/_start.asm
-cp ../peel/bin/* bin
-cp -rp ../peel/src/* src
-mkdir src/pulp
-cp -rp ../pulp/src/* src/pulp
-mkdir src/yc
-cp -rp ../yuzu/src/yc/*.yuzu src/yc
-mkdir src/zest
-cp -rp ../yuzu/src/zest/*.yuzu src/zest
-cd src
-../../peel/bin/jar.fap c source.jar
-../../peel/bin/juicer.fap c source.jar ../source.jz
-cd ..
-rm -rf src
-#mkdir src
-#mv source.jz src
-rm source.jz
 ../peel/bin/jar.fap c initrd.jar
 cd ..
 
